6107d38db0b5039548b9a7570d66cdc61e4774be
Rajajr <rajjr011@gmail.com>
2022-10-02 02:20:26 +0700

    UC-1 Create a Contacts in AddressBook

---
+++ AddressBooks/AddressBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBooks
+{
+    public class AddressBook
+    {
+        public static void AddPerson()
+        {
+            Console.WriteLine("1:Enter the First Name : ");
+            string FirstName=Console.ReadLine();
+            Console.WriteLine("2:Enter the Last Name : ");
+            string LastName = Console.ReadLine();
+            Console.WriteLine("3:Enter the State : ");
+            string State = Console.ReadLine();
+            Console.WriteLine("4:Enter the City : ");
+            string City = Console.ReadLine();
+            Console.WriteLine("5:Enter the Address : ");
+            string Address = Console.ReadLine();
+            Console.WriteLine("6:Enter the Zip Code : ");
+            string ZipCode = Console.ReadLine();
+            Console.WriteLine("7:Enter the PhoneNo : ");
+            string PhoneNumber = Console.ReadLine();
+            Console.WriteLine("8:Enter the Email-id : ");
+            string Email_id = Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ AddressBooks/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBooks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Address Book");
+            Console.WriteLine("Enter the Person details");
+            AddressBook.AddPerson();
+            AddressBook Person = new AddressBook();
+          
+        }
+        
+    }
+}
